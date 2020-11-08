647209145f356495f65780a7ba077f132e8bca4a
Jonas Cavalcanti <jonascavalcantineto@gmail.com>
2020-11-08 13:02:46 -0300

    ADD Java environment

---
+++ java/src/com/codelabs/codely/CodelyMain.java
@@ -0,0 +1,10 @@
+package com.codelabs.codely;
+
+public class CodelyMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ java/src/com/codelabs/rack/RackMain.java
@@ -0,0 +1,10 @@
+package com.codelabs.rack;
+
+public class RackMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
